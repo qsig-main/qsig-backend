cfa052a4ee50a4c09f1ab0d6194de4b3cfefcbfe
VioGarden <21ak191@queensu.ca>
2023-04-10 23:22:20 -0400

    tranq

--- home/views.py
+++ home/views.py
@@ -2,5 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-
-    return HttpResponse('Violet Evergarden')
+    return HttpResponse('Go to /admin')

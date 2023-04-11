9a46839b30024cbd21fa3ec14f2c25edfc508e66
VioGarden <21ak191@queensu.ca>
2023-04-10 23:51:12 -0400

    shingeki

--- home/views.py
+++ home/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse('Go to /admin')
+    return HttpResponse('Go to /admin for control panel')

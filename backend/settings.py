1df57a8274ad1148ceeaaff246cc1cd19ee858f6
qsig-main <97852272+qsig-main@users.noreply.github.com>
2026-03-18 14:57:09 -0400

    Update settings.py
    fixing trusted origins

--- backend/settings.py
+++ backend/settings.py
@@ -15,12 +15,9 @@ DEBUG = os.getenv('DEBUG')
 
 ALLOWED_HOSTS = ['*']
 
-CSRF_TRUSTED_ORIGINS = ['https://*.web-production-e20e7.up.railway.app',
-                        'https://*.127.0.0.1', 
-                        'https://*.web-production-9cae5.up.railway.app', 
-                        'https://*.web-production-e940.up.railway.app',
-                       'https://*.web-production-fda0.up.railway.app',
-                       'https://web-production-1acdf.up.railway.app/']
+CSRF_TRUSTED_ORIGINS = ['https://web-production-1acdf.up.railway.app/',
+                       'https://qsig.club',
+                       'https://qsig-frontend-psi.vercel.app']
 
 
 # Application definition
